ec91216bb90af1af9b27268ca0b043cbaaeb19fc
durgaprasadgajula <prasaddurga879@gmail.com>
2022-12-21 13:20:22 +0000

    first commit

---
+++ src/components/MovieItem/index.js
@@ -0,0 +1,42 @@
+// Write your code here
+import Popup from 'reactjs-popup'
+
+import ReactPlayer from 'react-player'
+import {IoMdClose} from 'react-icons/io'
+
+import 'reactjs-popup/dist/index.css'
+
+import './index.css'
+
+import {
+  ImageTrigger,
+  ModalContainer,
+  MoviePlayerContainer,
+  CloseButton,
+} from './styledComponents'
+
+const MovieItem = props => {
+  const {movieItem} = props
+  return (
+    <div>
+      <Popup
+        modal
+        trigger={<ImageTrigger src={movieItem.thumbnailUrl} alt="thumbnail" />}
+        className="popup-content"
+      >
+        {close => (
+          <ModalContainer>
+            <CloseButton type="button" onClick={() => close()}>
+              <IoMdClose size={20} color="#231f20" />
+            </CloseButton>
+            <MoviePlayerContainer>
+              <ReactPlayer url={movieItem.videoUrl} controls />
+            </MoviePlayerContainer>
+          </ModalContainer>
+        )}
+      </Popup>
+    </div>
+  )
+}
+
+export default MovieItem
